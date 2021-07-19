006c679e18396e73358e142d5c7940679750c8e3
LiudasBu <liudas.bucys@gmail.com>
2021-07-19 15:55:23 +0300

    Added tests

---
+++ tests/Controller/ConferenceControllerTest.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Tests\Controller;
+
+use Symfony\Component\Panther\PantherTestCase;
+
+class ConferenceControllerTest extends PantherTestCase
+{
+    public function testIndex()
+    {
+        $client = static::createClient();
+        $client->request('GET', '/');
+
+        $this->assertResponseIsSuccessful();
+        $this->assertSelectorTextContains('h2', 'Give your feedback');
+    }
+
+    public function testConferencePage()
+    {
+        $client = static::createClient();
+        $crawler = $client->request('GET', '/');
+
+        $this->assertCount(2, $crawler->filter('h4'));
+
+        $client->clickLink('View');
+
+        $this->assertPageTitleContains('Amsterdam');
+        $this->assertResponseIsSuccessful();
+        $this->assertSelectorTextContains('h2', 'Amsterdam 2019');
+        $this->assertSelectorExists('div:contains("There are 1 comments")');
+    }
+
+    public function testCommentSubmission()
+    {
+        $client = static::createClient();
+        $client->request('GET', '/conference/amsterdam-2019');
+        $client->submitForm('Submit', [
+            'comment_from[author]' => 'Fabien',
+            'comment_from[text]' => 'Some feedback from an automated functional test',
+            'comment_from[email]' => 'me@automat.ed',
+            'comment_from[photo]' => dirname(__DIR__, 2).'/public/images/under-construction.gif',
+        ]);
+        $this->assertResponseRedirects();
+        $client->followRedirect();
+        $this->assertSelectorExists('div:contains("There are 2 comments")');
+    }
+}
